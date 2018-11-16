e1fc9c7da68b9eb4c1c45772bc410ddd8f2dd1f1
Dorieke Berends <dorieke90@gmail.com>
2018-11-16 09:48:27 +0100

    Put the code for displaying the conversation in its own include file as I will be using it often

---
+++ resources/views/includes/recurring-elements/conversation.blade.php
@@ -0,0 +1,36 @@
+<div class="message-container">
+    @foreach ($conversation->messages as $message)
+        @if ($message->user_id == $auth->id)
+            <div class="sent-message message">
+                <p class="small-text"><b>You: </b></p>
+                <p class="small-text">{{ $message->message }}</p>
+                <p class="very-small-text grey-text">{{ $message->created_at }}</p>
+            </div>
+        @else 
+            <div class="received-message message">
+                <p class="small-text"><b>Other person: </b></p>
+                <p class="small-text">{{ $message->message }}</p>
+                <p class="very-small-text grey-text">{{ $message->created_at }}</p>
+            </div>
+        @endif
+    @endforeach
+</div>
+
+<div id="reply-form-container">
+    <form id="reply-form" method="POST" action="/conversations/{{ $conversation->id }}">
+        @csrf
+        @method('patch')
+
+        <div class="form-input">
+            <textarea class="text-input reply-textarea" name="message">Send a message</textarea>
+        </div>
+
+        <div>
+            <button type="submit">
+                <span class="im im-mail"></span>
+                &nbsp; Send
+            </button>
+        </div>
+
+    </form>
+</div>
